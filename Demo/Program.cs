cf9cd0bafff5219842016fc0e11a7593e46cc18f
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-05 15:13:09 +0200

    Demo test push

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Demo);
+            Console.WriteLine("Demo");
+            Console.WriteLine("Test push");
         }
     }
 }
