aa34ee6808d1328e8f76df134ec1fc48b3bd2045
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-06 17:19:50 +0200

    Q2

--- Assignment/Helper.cs
+++ Assignment/Helper.cs
@@ -9,13 +9,14 @@ namespace Assignment
 {
 	internal class Helper
 	{
-		public static void Reversed (ref ArrayList R)
+		#region Q1 
+		public static void Reversed(ref ArrayList R)
 		{
 			object temp = 0;
 			int C = R.Count - 1;
 			if (R is not null)
 			{
-				for(int i = 0; i < C-i ; i++)
+				for (int i = 0; i < C - i; i++)
 				{
 					temp = R[i];
 					R[i] = R[C - i];
@@ -23,6 +24,24 @@ namespace Assignment
 				}
 			}
 		}
+		#endregion
 
+		#region Q2 
+		public static List<int> EvenNumberInList(List<int> list)
+		{
+			List<int> result = new List<int>();
+
+			if (list is not null)
+			{
+				foreach (int num in list)
+				{
+					if (num % 2 == 0)
+						result.Add(num);
+				}
+			}
+			return result;
+		
+		}
+		#endregion
 	}
 }
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -10,15 +10,25 @@ namespace Assignment
 			Console.WriteLine("Test push");
 
 			#region Q1
-			ArrayList R = new ArrayList() { 1, 2, 3, 4 };
-			//ArrayList R = new ArrayList() { "d","c","s","w" };
-			//Console.WriteLine(R.Count);
-			foreach (int i in R)
-			{
-				Console.WriteLine(i);
-			}
-			Helper.Reversed(ref R);
-			foreach (int i in R)
+			//ArrayList R = new ArrayList() { 1, 2, 3, 4 };
+			////ArrayList R = new ArrayList() { "d","c","s","w" };
+			////Console.WriteLine(R.Count);
+			//foreach (int i in R)
+			//{
+			//	Console.WriteLine(i);
+			//}
+			//Helper.Reversed(ref R);
+			//foreach (int i in R)
+			//{
+			//	Console.WriteLine(i);
+			//}
+			#endregion
+
+			#region Q2 
+			List<int> list = new List<int>() { 2,3,4,5,6,7,8 };	
+			
+			List<int> newList = Helper.EvenNumberInList(list);
+			foreach (int i in newList)
 			{
 				Console.WriteLine(i);
 			}
