a16d81480d3e3df9ed34bc8d06cfab5da4064a4c
islam-refaei <i.refaei.refaei.saadawi@gmail.com>
2025-03-06 16:49:39 +0200

    Q1

---
+++ Assignment/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+	internal class Helper
+	{
+		public static void Reversed (ref ArrayList R)
+		{
+			object temp = 0;
+			int C = R.Count - 1;
+			if (R is not null)
+			{
+				for(int i = 0; i < C-i ; i++)
+				{
+					temp = R[i];
+					R[i] = R[C - i];
+					R[C - i] = temp;
+				}
+			}
+		}
+
+	}
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment
+﻿using System.Collections;
+
+namespace Assignment
 {
 	internal class Program
 	{
@@ -6,6 +8,21 @@
 		{
 			Console.WriteLine("Assignment");
 			Console.WriteLine("Test push");
+
+			#region Q1
+			ArrayList R = new ArrayList() { 1, 2, 3, 4 };
+			//ArrayList R = new ArrayList() { "d","c","s","w" };
+			//Console.WriteLine(R.Count);
+			foreach (int i in R)
+			{
+				Console.WriteLine(i);
+			}
+			Helper.Reversed(ref R);
+			foreach (int i in R)
+			{
+				Console.WriteLine(i);
+			}
+			#endregion
 		}
 	}
 }
